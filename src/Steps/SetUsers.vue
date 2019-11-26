51cacddd21fb2197db90c245fc6f00b3f82572cf
Henry Rossiter <henry.rossiter@utexas.edu>
2019-11-26 12:39:46 -0600

    continue bug fix, update changelog and version

--- src/Steps/SetUsers.vue
+++ src/Steps/SetUsers.vue
@@ -64,6 +64,9 @@ export default {
     currentApplet() {
       return this.$store.state.currentApplet;
     },
+    readyToContinue() {	
+      return true;
+    },
   },
   watch: {
     isUsersLoaded() {
