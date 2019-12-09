575a1c644a7c95aea7569384decd8db9bddf9610
Henry Rossiter <henry.rossiter@utexas.edu>
2019-12-09 15:30:09 -0600

    enrich activity set builder ui

--- src/Components/Builder/Builder.vue
+++ src/Components/Builder/Builder.vue
@@ -1,5 +1,38 @@
 <template>
-  <ActivitySetBuilder />
+  <div>
+    <v-toolbar
+      app
+      dark
+      color="primary"
+      clipped-left
+    >
+      <v-toolbar-side-icon
+        @click="toggleDrawer"  
+      />
+      <v-toolbar-title>Activity Set Builder</v-toolbar-title>
+    </v-toolbar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>delete</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Reset Builder</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <ActivitySetBuilder />
+    </v-content>
+  </div>
 </template>
 
 <script>
@@ -10,5 +43,15 @@ export default {
   components: {
     ...Components
   },
+  data () {
+    return {
+      drawer: false,
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
 }
 </script>
