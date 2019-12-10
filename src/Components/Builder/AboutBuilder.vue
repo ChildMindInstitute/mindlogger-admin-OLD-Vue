06d3cd3664fac05ed522a72c79d0fd7190c1553e
Henry Rossiter <henry.rossiter@utexas.edu>
2019-12-10 08:54:13 -0600

    lint

--- src/Components/Builder/AboutBuilder.vue
+++ src/Components/Builder/AboutBuilder.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <p>
-    This tool is used to design and save ReproNim activity sets. Saved activity sets can be uploaded to MindLogger through the Admin Panel.
-    For more information on ReproNim, please visit the official <a href="https://github.com/ReproNim/reproschema">Github</a> repository.
+      This tool is used to design and save ReproNim activity sets. Saved activity sets can be uploaded to MindLogger through the Admin Panel.
+      For more information on ReproNim, please visit the official <a href="https://github.com/ReproNim/reproschema">Github</a> repository.
     </p>
   </v-container>
 </template>
