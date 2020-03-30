0f1c24b878afb567acad95b45f0135921b31113b
Henry Rossiter <henry.rossiter@utexas.edu>
2020-03-29 19:59:24 -0500

    3 more mutation unit tests

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -3,11 +3,54 @@ import Vuex from 'vuex';
 import { storeConfig } from '../../src/State/state.js';
 import { cloneDeep } from 'lodash';
 
-test('sets "backend" value when "setBackend" is committed', () => {
+function getLocalStore() {
   const localVue = createLocalVue();
   localVue.use(Vuex);
   const store = new Vuex.Store(cloneDeep(storeConfig));
+  return store;
+}
+
+test('sets "backend" value when "setBackend" is committed', () => {
+  const store = getLocalStore();
   const sampleBackend = 'sample.backend.com/api/v1';
   store.commit('setBackend', sampleBackend);
   expect(store.state.backend).toBe(sampleBackend);
+});
+
+test('sets "currentApplet" value when "setCurrentApplet" is committed', () => {
+  const store = getLocalStore();
+  const sampleApplet = {
+    'applet': {
+      '_id': 'testAppletId',
+    }
+  };
+  store.commit('setCurrentApplet', sampleApplet);
+  expect(store.state.currentApplet).toBe(sampleApplet);
+});
+
+test('sets "allApplets" value when "setAllApplets" is committed', () => {
+  const store = getLocalStore();
+  const sampleAppletArray = [
+    {
+      'applet': {
+        '_id': 'testAppletId1',
+      },
+    },
+    {
+      'applet': {
+        '_id': 'testAppletId2',
+      },
+    },
+  ];
+  store.commit('setAllApplets', sampleAppletArray);
+  expect(store.state.allApplets).toBe(sampleAppletArray);
+});
+
+test('sets "auth" value when "setAuth" is committed', () => {
+  const store = getLocalStore();
+  const sampleAuth = {
+    'token': 'xxx',
+  };
+  store.commit('setAuth', sampleAuth);
+  expect(store.state.auth).toBe(sampleAuth);
 });
