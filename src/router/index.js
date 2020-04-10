b179a6b33ab80e6d7205993e2bcbce27821d5bf2
henry <henry.rossiter@utexas.edu>
2020-04-10 10:54:01 -0500

    beef up router

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,56 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Stepper from '@/Components/Stepper.vue';
 import Builder from '@/Components/Builder/Builder.vue';
+import Login from '../Steps/Login';
+import SetApplet from '../Steps/SetApplet';
+import SetUsers from '../Steps/SetUsers';
+import SetSchedule from '../Steps/SetSchedule';
+
+import _ from 'lodash';
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/build',
       name: 'Builder',
       component: Builder,
+      meta: {
+        requiresAuth: true
+      },
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+          guest: true
+      }
+    },
+    {
+      path: '/applets',
+      name: 'SetApplet',
+      component: SetApplet,
+      meta: {
+        requiresAuth: true
+      },
+    },
+    {
+      path: '/users',
+      name: 'SetUsers',
+      component: SetUsers,
+      meta: {
+        requiresAuth: true
+      },
+    },
+    {
+      path: '/schedules',
+      name: 'SetSchedule',
+      component: SetSchedule,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/',
@@ -18,4 +59,32 @@ export default new Router({
       component: Stepper,
     },
   ]
+});
+
+
+router.beforeEach((to, from, next) => {
+  const isNotLoggedIn = _.isEmpty(this.$store.state.auth);
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+      if (isNotLoggedIn) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          });
+      } else {
+          next();
+      }
+  } else if (to.matched.some(record => record.meta.guest)) {
+      if (isNotLoggedIn) {
+          next();
+      }
+      else {
+          next({
+            path: '/applets'
+          });
+      }
+  } else {
+      next();
+  }
 })
+
+export default router
