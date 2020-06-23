8653f36591d571768889c96d7c887c44139454ce
ProNinoGit120 <bestmatch19940123@gmail.com>
2020-06-23 09:41:53 -0400

    Updated LoginForm for account owner role

--- src/Components/Authentication/LoginForm.vue
+++ src/Components/Authentication/LoginForm.vue
@@ -106,7 +106,7 @@ export default {
         this.$store.commit('setAuth', {auth: resp.data, email: this.email});
         this.$router.push('/applets')
       }).catch((e) => {
-          this.error = e.response.data.message;
+        this.error = e.response.data.message;
       });
     },
     onCreateAccount() {
