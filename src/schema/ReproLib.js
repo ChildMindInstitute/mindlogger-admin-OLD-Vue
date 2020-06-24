acbb4d2f3822167b17ab364233d88526f8a8f06c
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2020-06-24 04:20:30 -0400

    Add initial schema definitions for ReproLib, SKOS and Schema.org

---
+++ src/schema/ReproLib.js
@@ -0,0 +1,6 @@
+export default {
+  inputType: 'reprolib:terms/inputType',
+  valueType: 'reprolib:terms/valueType',
+  multipleChoice: 'reprolib:terms/multipleChoice',
+  responseOptions: 'reprolib:terms/responseOptions',
+};
