77c71839996b99bd04961d45b2fe514861818086
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2020-07-06 13:42:44 -0400

    Add initial setup for sentry logging.
    Issue #219

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import DaySpanVuetify from 'dayspan-vuetify-2';
+import VuetifyDialog from 'vuetify-dialog';
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
 import router from './router';
 import App from './App.vue';
-import VuetifyDialog from 'vuetify-dialog';
 import vuetify from './plugins/vuetify';
 import "vuetify-dialog/dist/vuetify-dialog.min.css";
 
@@ -10,6 +13,17 @@ import "vuetify-dialog/dist/vuetify-dialog.min.css";
 //   theme: {dark: true}
 // })
 Vue.config.productionTip = false;
+const environment = process.env.NODE_ENV || 'development';
+
+// Avoid using sentry for development since it is not needed and it would waste
+// the log entries quota.
+if (environment !== 'development') {
+  Sentry.init({
+    environment,
+    dsn: 'https://3ee572fdc2af4938a5569d62d8389996@o414302.ingest.sentry.io/5313178',
+    integrations: [new VueIntegration({Vue, attachProps: true})],
+  });
+}
 
 Vue.use(DaySpanVuetify, {
   data: {
