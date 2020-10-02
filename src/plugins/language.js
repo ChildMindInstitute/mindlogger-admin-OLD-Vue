522fa47ac15ba266e0711f160dab7b79fe2e074b
Benjamin Thonet <devtech728@gmail.com>
2020-10-02 15:19:50 +0300

    language file

--- src/plugins/language.js
+++ src/plugins/language.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import VueI18n from "vue-i18n";
-import store from "../State/state";
-import _ from "lodash";
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import store from '../State/state';
+import _ from 'lodash';
 
-import en_US from "./locales/en_US";
-import fr_FR from "./locales/fr_FR";
+import en_US from './locales/en_US';
+import fr_FR from './locales/fr_FR';
 
 Vue.use(VueI18n);
 
@@ -13,18 +13,18 @@ const browserLanguage = (() => {
   const language = navigator.language || navigator.userLanguage;
 
   if (
-    ["fr", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "fr-LU", "fr-MC"].includes(
+    ['fr', 'fr-BE', 'fr-CA', 'fr-CH', 'fr-FR', 'fr-LU', 'fr-MC'].includes(
       language
     )
   ) {
-    return "fr_FR";
+    return 'fr_FR';
   }
 
-  return "en_US";
+  return 'en_US';
 })();
 
 // check if a language setting is stored already
-const locale = _.get(store, "state.currentLanguage", browserLanguage);
+const locale = _.get(store, 'state.currentLanguage', browserLanguage);
 
 // new instance
 export default new VueI18n({
