c992bb98b93ca6cfadcbd3e6299a8936199d07ef
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2020-10-05 19:50:21 -0300

    Fix labels and add tooltip for token chart.
    Issue #207

---
+++ src/core/slugify.js
@@ -0,0 +1,14 @@
+/**
+ * Converts a text to a slug.
+ *
+ * @param {string} text the text to be converted to a slug.
+ * @returns {string} the resulting slug.
+ */
+export default function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
