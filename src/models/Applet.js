1405b8acc9c163f414a3561cac6df41cecaf7b6d
mtg137 <jony374@outlook.com>
2021-03-21 16:59:42 -0400

    show tooltip when clicking response

--- src/models/Applet.js
+++ src/models/Applet.js
@@ -293,7 +293,7 @@ export default class Applet {
       activity.initSubScaleItems();
 
       const activityId = activity.data._id.split('/')[1];
-      activity.addSubScaleValues(this.subScales[activityId] || {});
+      await activity.addSubScaleValues(this.subScales[activityId] || {});
     }
   }
 
