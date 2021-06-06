1bc4f7125741131dbee65a47c53d69d70198ab76
devbtech <devtech728@gmail.com>
2021-06-06 12:50:45 +0500

    MRN changed to secret_user_id

--- src/Components/Utils/mixins/AppletMixin.js
+++ src/Components/Utils/mixins/AppletMixin.js
@@ -72,8 +72,7 @@ export const AppletMixin = {
           });
         }
 
-        return api
-          .getUsersData({
+        return api.getUsersData({
             apiHost: this.$store.state.backend,
             token: this.$store.state.auth.authToken.token,
             appletId: appletId,
@@ -256,7 +255,7 @@ export const AppletMixin = {
                 activity_start_time: response.responseStarted || null,
                 activity_end_time: response.responseCompleted || null,
                 flag,
-                MRN: MRN || null,
+                secret_user_id: MRN || null,
                 userId: _id,
                 activity_id: response.activity['@id'],
                 activity_name: activity.label.en,
@@ -288,7 +287,7 @@ export const AppletMixin = {
               'activity_start_time',
               'activity_end_time',
               'flag',
-              'MRN',
+              'secret_user_id',
               'userId',
               'activity_id',
               'activity_name',
