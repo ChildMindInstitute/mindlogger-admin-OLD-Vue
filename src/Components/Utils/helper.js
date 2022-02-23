7ee82ead7b7202c0b58b864277216cf0abaadd73
jDev <rjtech0018@gmail.com>
2022-02-22 22:40:00 -0500

    fix parsing variable names

--- src/Components/Utils/helper.js
+++ src/Components/Utils/helper.js
@@ -11,6 +11,16 @@ export const getTextBetweenBrackets = (str) => {
 };
 
 export const replaceItemVariableWithName = (markdown, items, answers) => {
+  const getTimeString = (obj) => {
+    if (!obj) return '';
+
+    return `${obj.hour.toString().padStart(2, '0')}:${obj.minute.toString().padStart(2, '0')}`;
+  }
+
+  const getDateString = (obj) => {
+    if (!obj) return '';
+    return `${obj.year}-${Number(obj.month+1).toString().padStart(2, '0')}-${Number(obj.day).toString().padStart(2, '0')}`;
+  }
   try {
     const variableNames = getTextBetweenBrackets(markdown);
     if (variableNames && variableNames.length) {
@@ -20,32 +30,46 @@ export const replaceItemVariableWithName = (markdown, items, answers) => {
           if (val && val.id === variableName) index = key;
         });
 
+        const reg = new RegExp(`\\[\\[${variableName}\\]\\]`, "gi");
         if (answers[index] && Array.isArray(answers[index].value)) {
           let names = [];
           answers[index].value.forEach(ans => {
             const item = _.find(items[index] && items[index].responseOptions, { value: ans });
             if (item) names.push(item.name.en);
           })
-          const reg = new RegExp(`${variableName}`, "gi");
           markdown = markdown.replace(reg, names.join(', '));
 
         } else if (typeof answers[index] === "object") {
-          const item = _.find(items[index] && items[index].responseOptions, answers[index]);
-          if (item || answers[index]) {
-            const reg = new RegExp(`${variableName}`, "gi");
-            markdown = markdown.replace(reg, (item && item.name && item.name.en) || (answers[index] && answers[index].value));
+          switch (items[index].inputType) {
+            case 'radio':
+              const item = index > -1 && _.find(items[index].responseOptions, { value: answers[index].value });
+              if (item) {
+                markdown = markdown.replace(reg, item.name.en);
+              }
+              break;
+            case 'slider':
+              markdown = markdown.replace(reg, answers[index].value);
+              break;
+            case 'timeRange':
+              markdown = markdown.replace(reg, getTimeString(answers[index].value && answers[index].value.from) + ' - ' + getTimeString(answers[index].value && answers[index].value.to));
+              break;
+            case 'date':
+              markdown = markdown.replace(reg, getDateString(answers[index].value));
+              break;
+            case 'ageSelector':
+              markdown = markdown.replace(reg, answers[index].value);
+              break;
           }
-
         } else if (answers[index]) {
-          const reg = new RegExp(`${variableName}`, "gi");
           markdown = markdown.replace(reg, answers[index]);
         }
+
+        markdown = markdown.replace(reg, '');
       });
     }
-    markdown = markdown.replace(/[\[\]']+/g, '');
-
   } catch (error) {
     console.warn(error)
   }
+
   return markdown;
 }
