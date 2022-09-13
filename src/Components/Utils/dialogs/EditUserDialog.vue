b2aa1351ecd49c78b9d57e9472f69597b2a89dc4
iradchenko <iradchenko@scnsoft.com>
2022-09-13 15:20:38 +0300

    1778 Fix 'edit user data' dialog validation

--- src/Components/Utils/dialogs/EditUserDialog.vue
+++ src/Components/Utils/dialogs/EditUserDialog.vue
@@ -4,27 +4,30 @@
     max-width="500px"
     @input="$emit('input', $event)"
   >
-    <v-card
-      class="pa-4"
-    >
+    <v-card class="pa-4">
       <v-card-title class="text-center">
-        {{ $t('editUserData') }}
+        {{ $t("editUserData") }}
       </v-card-title>
 
-      <v-card-text>
-        <v-text-field
-          v-model="params.nickName"
-          :label="$t('nickName')"
-        />
+      <v-form
+        ref="form"
+        v-model="valid"
+      >
+        <v-card-text>
+          <v-text-field
+            v-model="params.nickName"
+            :label="$t('nickName')"
+          />
 
-        <v-text-field
-          key="secret-id-input"
-          v-model="params.MRN"
-          :label="$t('secretUserId')"
-          :rules="secretUserIdRules"
-          required
-        />
-      </v-card-text>
+          <v-text-field
+            key="secret-id-input"
+            v-model="params.MRN"
+            :label="$t('secretUserId')"
+            :rules="secretUserIdRules"
+            required
+          />
+        </v-card-text>
+      </v-form>
 
       <v-card-actions>
         <v-spacer />
@@ -34,14 +37,15 @@
           text
           @click="$emit('input', false)"
         >
-          {{ $t('close') }}
+          {{ $t("close") }}
         </v-btn>
         <v-btn
           color="primary"
+          :disabled="!valid"
           text
           @click="updateProfile"
         >
-          {{ $t('save') }}
+          {{ $t("save") }}
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -49,9 +53,9 @@
 </template>
 
 <style scoped>
-  .text-center {
-    text-align: center;
-  }
+.text-center {
+  text-align: center;
+}
 </style>
 
 <script>
@@ -74,28 +78,29 @@ export default {
 
   data() {
     return {
-      secretUserIdRules: [
-        (v) => !!v || this.$i18n.t("secretUserIdRequired")
-      ],
+      valid: true,
+      secretUserIdRules: [(v) => !!v || this.$i18n.t("secretUserIdRequired")],
       params: {
         MRN: this.user.MRN,
         nickName: this.user.nickName,
         userId: this.user._id,
-      }
-    }
+      },
+    };
   },
 
   methods: {
-    updateProfile () {
-      api.updateProfile({
-        apiHost: this.apiHost,
-        token: this.token,
-        appletId: this.currentAppletMeta.id,
-        options: this.params
-      }).then(() => {
-        this.$emit('updated');
-      })
-    }
-  }
-}
+    updateProfile() {
+      api
+        .updateProfile({
+          apiHost: this.apiHost,
+          token: this.token,
+          appletId: this.currentAppletMeta.id,
+          options: this.params,
+        })
+        .then(() => {
+          this.$emit("updated");
+        });
+    },
+  },
+};
 </script>
