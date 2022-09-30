4343591ff691b02ddbc32ed190d0289eb58ab959
iradchenko <iradchenko@scnsoft.com>
2022-09-30 13:01:33 +0300

    fix: #1300 Export is not downloadable for some applets

--- src/Components/Utils/encryption/encryption.vue
+++ src/Components/Utils/encryption/encryption.vue
@@ -40,12 +40,15 @@ const getAESKey = ( appletPrivateKey, userPublicKey, appletPrime, base ) => {
 const decryptData = ({ text, key }) => {
   let textParts = text.split(':');
   let iv = Buffer.from(textParts.shift(), 'hex');
-  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  let encryptedText = Buffer.from(textParts.join(':'), 'hex');  
   let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
   let decrypted = decipher.update(encryptedText);
-
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
+  
+  try {
+    return decrypted.toString() + decipher.final('utf8');
+  } catch(error) {
+    return JSON.stringify([{type: "", time: "", screen: ""}])
+  }
 }
 
 /** encrypt */
